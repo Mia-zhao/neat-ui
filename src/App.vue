65bbdbf2af7ab7d306c2b67eac7426d161584f1c
Mia-zhao <miazhaouw@gmail.com>
2020-09-16 22:01:36 +0800

    initialize vue router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+<div>导航栏 | <router-link to="/">HelloWorld</router-link>
+  | <router-link to="/2">HelloWorld2</router-link>
+</div>
+<router-view />
 </template>
 
 <script>
@@ -8,8 +10,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
