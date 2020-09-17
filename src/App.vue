5e61ae8473537331b523ef14b0ebfcdec84999fb
Mia-zhao <miazhaouw@gmail.com>
2020-09-17 17:42:50 +0800

    set menu visibility after doc router

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,18 @@
 <script lang="ts">
 import { ref, provide } from 'vue'
 import { MENU_VISIBLE } from './constants/Refs'
+import { router } from './router'
 
 export default {
   name: 'App',
   setup() {
     const screenWidth = document.documentElement.clientWidth
-    const menuVisible = ref(screenWidth > 500)
+    const showMenu = screenWidth > 500
+    const menuVisible = ref(showMenu)
     provide(MENU_VISIBLE, menuVisible)
+    router.afterEach(() => {
+      menuVisible.value = showMenu
+    })
   }
 }
 </script>
---
+++ src/router.ts
@@ -0,0 +1,22 @@
+import { createWebHashHistory, createRouter } from 'vue-router'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
+import SwitchDoc from './components/SwitchDoc.vue'
+
+const history = createWebHashHistory()
+export const router = createRouter({
+  history,
+  routes: [
+    {
+      path: '/', component: Home
+    },
+    {
+      path: '/doc', component: Doc,
+      children: [
+        {
+          path: 'switch', component: SwitchDoc
+        }
+      ]
+    }
+  ]
+})
