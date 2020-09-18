27f69292c9b44aaaf68440d4bccfa196f553f80d
Mia-zhao <miazhaouw@gmail.com>
2020-09-18 17:45:47 +0800

    add Button component

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
     Button DOC
+    <h1></h1>
+    <Button>HI</Button>
   </div>
 </template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue'
+export default {
+  components: { Button }
+}
+</script>
