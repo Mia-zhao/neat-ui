8bdbfcb517caaa29accdb00c4ebc593466b780ad
Mia-zhao <miazhaouw@gmail.com>
2020-09-19 18:24:41 +0800

    bind attrs to button

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,14 +1,32 @@
 <template>
-  <div>
-    Button DOC
-    <h1></h1>
-    <Button>HI</Button>
-  </div>
+  <h1>Button</h1>
+  <section class="description">
+    <p>This is a button component.</p>
+  </section>
+  <h2>Examples</h2>
+  <section class="examples">
+    <div class="example-basic">
+      <h3>Basic Usage</h3>
+      <Button @click="onClick"
+        @focus="onClick" @mouseover="onClick">
+        你好
+      </Button>
+    </div>
+  </section>
+  <section class="properties">
+
+  </section>
 </template>
 
 <script lang="ts">
 import Button from '../lib/Button.vue'
 export default {
-  components: { Button }
+  components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log('1')
+    }
+    return { onClick }
+  }
 }
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,11 @@
 <template>
-  <button>
+  <button v-bind="$attrs" >
     <slot />
   </button>
 </template>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false
+}
+</script
