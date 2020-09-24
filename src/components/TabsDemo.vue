47cbf7ade33ddaf71a62f50f5b1c56801c729e5a
Mia-zhao <miazhaouw@gmail.com>
2020-09-24 12:54:00 +0800

    add Tabs

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>
-    Tabs DOC
-  </div>
+  <h1>Tabs</h1>
+  <section class="description">
+    <p>This is Tabs component.</p>
+  </section>
+  <h2>Examples</h2>
+  <section class="examples">
+    <div class="example-tabs">
+      <Tabs >
+        <Tab title="Nav 1">Content 1</Tab>
+        <Tab title="Nav 2">Content 2</Tab>
+      </Tabs>
+    </div>
+  </section>
+  <section class="properties">
+
+  </section>
 </template>
+
+<script lang="ts">
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+export default {
+  components: {
+    Tabs,
+    Tab
+  }
+}
+</script>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Tabs 组件
+    <component :is="defaultSlots[0]" />
+    <component :is="defaultSlots[1]" />
+  </div>
+</template>
+
+<script lang="ts">
+import Tab from './Tab.vue'
+export default {
+  setup(props, context) {
+    const defaultSlots = context.slots.default()
+    defaultSlots.forEach(slot => {
+      if (slot.type !== Tab) {
+        throw new Error('Children of Tabs must be of type Tab')
+      }
+    })
+    return { defaultSlots }
+  }
+}
+</script>
