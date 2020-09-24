8062c6340d5088c304c10ab0b216a55f4e0581d0
Mia-zhao <miazhaouw@gmail.com>
2020-09-24 23:35:49 +0800

    init Carousel component

--- src/components/CarouselDemo.vue
+++ src/components/CarouselDemo.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
-    Carousel DOC
-  </div>
+  <h1>Carousel</h1>
+  <section class="description">
+    <p>This is a Carousel component.</p>
+  </section>
+  <h2>Examples</h2>
+  <section class="examples">
+    <h3>Carousel Default</h3>
+    <div class="example-carousel">
+      <Carousel />
+    </div>
+  </section>
+  <section class="properties">
+
+  </section>
 </template>
+
+<script lang="ts">
+import Carousel from '../lib/Carousel.vue'
+export default {
+  components: {
+    Carousel
+  }
+}
+</script>
