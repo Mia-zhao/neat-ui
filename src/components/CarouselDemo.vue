1f2d7b12c39e7f8845447fbb83769db955c0b031
Mia-zhao <miazhaouw@gmail.com>
2020-09-25 09:12:03 +0800

    add Carousel slide style

--- src/components/CarouselDemo.vue
+++ src/components/CarouselDemo.vue
@@ -6,8 +6,13 @@
   <h2>Examples</h2>
   <section class="examples">
     <h3>Carousel Default</h3>
-    <div class="example-carousel">
-      <Carousel />
+    <div class="example-carousel1">
+      <Carousel v-model:selectedIndex="selectedIndex">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </Carousel>
     </div>
   </section>
   <section class="properties">
@@ -16,10 +21,15 @@
 </template>
 
 <script lang="ts">
+import { ref } from 'vue'
 import Carousel from '../lib/Carousel.vue'
 export default {
   components: {
     Carousel
+  },
+  setup() {
+    const selectedIndex = ref<Number>(0);
+    return { selectedIndex }
   }
 }
 </script>
--- src/lib/Carousel.vue
+++ src/lib/Carousel.vue
@@ -1,5 +1,58 @@
 <template>
-  <div>
-    Carousel component
+  <div class="neat-carousel">
+    <div class="neat-carousel-content">
+
+    </div>
+    <div class="neat-carousel-slide">
+      <div class="neat-carousel-slide-item"
+        v-for="(slot, index) in slots" :key="index"
+        @click="$emit('update:selectedIndex', index)"
+        :class="{active: index === selectedIndex}">
+      </div>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+export default {
+  props: {
+    slideStyle: {
+      type: String,
+      default: 'dot'
+    },
+    autoPlay: {
+      type: Boolean,
+      default: false
+    },
+    selectedIndex: {
+      type: Number
+    }
+  },
+  setup(props, context) {
+    const slots = context.slots.default()
+    return { slots }
+  }
+}
+</script>
+
+<style lang="scss">
+@import './neat-style.scss';
+.neat-carousel {
+  &-slide {
+    display: flex;
+    justify-content: center;
+    &-item {
+      margin: 0 4px;
+      width: 16px;
+      height: 5px;
+      border-radius: $radius;
+      background: $color-grey-400;
+      transition: all 250ms;
+      &.active {
+        background: black;
+        width: 32px;
+      }
+    }
+  }
+}
+</style>
