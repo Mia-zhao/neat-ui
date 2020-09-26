8683ad5ab14d08cb2c394444bb6d0dcbc08a8252
Mia-zhao <miazhaouw@gmail.com>
2020-09-26 20:40:58 +0800

    add sub menu style

--- src/components/MenuDemo.vue
+++ src/components/MenuDemo.vue
@@ -5,10 +5,28 @@
   </section>
   <h2>Examples</h2>
   <section class="examples">
-    <div class="example-menu">
-      <h3>Menu</h3>
+    <div class="example-menu1">
+      <h3>Menu1</h3>
       <Menu>
         <Submenu title="SubMenu 1">
+          <Menuitem>Menu 1</Menuitem>
+          <Menuitem>Menu 2</Menuitem>
+        </Submenu>
+      </Menu>
+    </div>
+    <div class="example-menu2">
+    <h3>Menu2</h3>
+      <Menu>
+        <Submenu hideArrow title="SubMenu 2">
+          <Menuitem>Menu 1</Menuitem>
+          <Menuitem>Menu 2</Menuitem>
+        </Submenu>
+      </Menu>
+    </div>
+    <div class="example-menu3">
+    <h3>Menu2</h3>
+      <Menu>
+        <Submenu :collapsible="false" title="SubMenu 3">
           <Menuitem>Menu 1</Menuitem>
           <Menuitem>Menu 2</Menuitem>
         </Submenu>
--- src/lib/SubMenu.vue
+++ src/lib/SubMenu.vue
@@ -1,32 +1,109 @@
 <template>
-  <div>
-    <div class="neat-sub-menu-title">
-      {{ title }}
+  <div class="neat-sub-menu">
+    <div class="neat-sub-menu-title"
+      :class="{collapsed: collapsed}"
+      @click="collapsible && (collapsed = !collapsed)">
+      <span>
+        {{ title }}
+      </span>
+      <svg class="icon" aria-hidden="true"
+        v-if="collapsible && !hideArrow">
+        <use :xlink:href="`#icon-arrow`"></use>
+      </svg>
     </div>
-    <div class="neat-sub-menu-children">
+    <div class="neat-sub-menu-children"
+      :class="{collapsed: collapsed}">
       <component class="neat-menu-item"
+        :class="{selected: selectedIndex===index}"
         v-for="(slot, index) in slots" :key="index"
+        @click="selectedIndex=index"
         :is="slot"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue'
 import Menuitem from './MenuItem.vue'
 export default {
   props: {
     title: {
       type: String
+    },
+    defaultCollapsed: {
+      type: Boolean,
+      default: true
+    },
+    collapsible: {
+      type: Boolean,
+      default: true
+    },
+    hideArrow: {
+      type: Boolean,
+      default: false
     }
   },
   setup(props, context) {
+    const collapsed = ref<Boolean>(props.defaultCollapsed)
+    const selectedIndex = ref<Number>(null)
     const slots = context.slots.default()
     slots.forEach(slot => {
       if (slot.type !== Menuitem) {
         throw new Error('Children of SubMenu must be of type or MenuItem')
       }
     })
-    return { slots }
+    return { slots, collapsed, selectedIndex }
   }
 }
 </script>
+
+<style lang="scss">
+@import './neat-style.scss';
+.neat-sub-menu {
+  max-width: 300px;
+  color: $color-grey-600;
+  > .neat-sub-menu-title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+    padding: 4px 8px;
+    margin: 4px 0;
+    > span {
+      font-size: 16px;
+    }
+    > .icon {
+      fill: $color-grey-600;
+      width: 12px;
+      height: 12px;
+    }
+    &.collapsed, &:hover {
+      > span {
+        color: $color-lightblue-700;
+      }
+      > .icon {
+        fill: $color-lightblue-700;
+      }
+    }
+    &.collapsed > .icon {
+      transform: rotateX(180deg);
+    }
+  }
+  > .neat-sub-menu-children {
+    display: none;
+    &.collapsed {
+      display: block;
+    }
+    > .neat-menu-item {
+      font-size: 14px;
+      padding: 4px 24px;
+      margin: 4px 0;
+      cursor: pointer;
+      &.selected {
+        color: $color-lightblue-700;
+        background: $color-lightblue-100;
+      }
+    }
+  }
+}
+</style>
